90dd0e4991a0807f195fefbf6866bcb64c4f5058
Rodrigo Retamal <retamalpeirano@gmail.com>
2024-11-23 16:05:30 -0300

    templates básicas listas

--- ecommerce_app/views.py
+++ ecommerce_app/views.py
@@ -3,13 +3,17 @@ from store.models import Product, ReviewRating
 
 def home(request):
     products = Product.objects.filter(is_available=True).order_by('created_date')
+    product_reviews = []
 
     for product in products:
         reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
+        product_reviews.append({
+            'product': product,
+            'reviews': reviews,
+        })
 
     context = {
-        'products': products,
-        'reviews': reviews,
+        'product_reviews': product_reviews,
     }
 
     return render(request, 'index.html', context)
