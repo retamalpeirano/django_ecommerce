3f446996f4b3e114ad7b430c0f18b925fb2ab03e
Rodrigo Retamal <retamalpeirano@gmail.com>
2024-11-23 13:28:10 -0300

    tontera

--- store/urls.py
+++ store/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('category/<slug:category_slug>/', views.store, name="products_by_category"),  # Productos por categoría
     path('product/<slug:product_slug>/', views.product_detail, name="product_detail"),  # Detalles del producto
     path('search/', views.search, name='search'),  # Búsqueda
-    ppath('submit_review/<int:product_id>/', views.submit_review, name='submit_review'), # Reviews
+    path('submit_review/<int:product_id>/', views.submit_review, name='submit_review'), # Reviews
 ]
