92d4f94ac57634881d574b8424ef735b4b654c6b
Rodrigo Retamal <retamalpeirano@gmail.com>
2024-11-23 15:35:41 -0300

    url index

--- ecommerce_app/views.py
+++ ecommerce_app/views.py
@@ -4,8 +4,12 @@ from store.models import Product, ReviewRating
 def home(request):
     products = Product.objects.filter(is_available=True).order_by('created_date')
 
+    for product in products:
+        reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
+
     context = {
         'products': products,
+        'reviews': reviews,
     }
 
-    return render(request, 'home.html', context)
+    return render(request, 'index.html', context)
