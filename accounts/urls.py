4216465cf4f39d41884dd7129d69fc1fa0d99c60
Rodrigo Retamal <retamalpeirano@gmail.com>
2024-11-28 19:20:16 -0300

    ojala sirva

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from .views import profile, CustomSignupView
+from django.contrib.auth.views import PasswordResetView
+
+app_name = 'accounts'
 
 urlpatterns = [
-    path('', include('allauth.urls')),  # Rutas de Allauth
-    path('profile/', profile, name='profile'),  # Rutas app accounts
+    path('', include('allauth.urls')),
     path('signup/', CustomSignupView.as_view(), name='custom_signup'),
+    path('reset-password/', PasswordResetView.as_view(), name='reset_password'),
 ]
--- ecommerce_app/urls.py
+++ ecommerce_app/urls.py
@@ -8,7 +8,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('accounts/', include('accounts.urls')),  
+    path('accounts/', include('accounts.urls')),
     path('store/', include('store.urls')),
     path('dashboard/', views.dashboard, name='dashboard'),
 ]
