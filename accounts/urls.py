19951147795e2f5f50552619cf27e162409be9c8
Rodrigo Retamal <retamalpeirano@gmail.com>
2024-11-28 20:34:51 -0300

    2

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from .views import profile, CustomSignupView
-from django.contrib.auth.views import PasswordResetView
+from django.contrib.auth.views import PasswordResetView, PasswordChangeView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('', include('allauth.urls')),
+    path('profile/', profile, name='profile'),
     path('signup/', CustomSignupView.as_view(), name='custom_signup'),
-    path('reset-password/', PasswordResetView.as_view(), name='reset_password'),
+    path('reset-password/', PasswordResetView.as_view(template_name='accounts/resetPassword.html'), name='reset_password'),
+    path('change-password/', PasswordChangeView.as_view(template_name='accounts/change_password.html'), name='change_password'),
 ]
