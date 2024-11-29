31410ca11b9e7eced777c6c2776208c177a8cbe9
Rodrigo Retamal <retamalpeirano@gmail.com>
2024-11-29 19:07:17 -0300

    1

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,7 @@ app_name = 'accounts'
 urlpatterns = [
     path('', include('allauth.urls')),
     path('profile/', profile, name='profile'),
-    path('signup/', CustomSignupView.as_view(), name='account_signup'),
+    #path('signup/', CustomSignupView.as_view(), name='account_signup'),
     path('reset-password/', PasswordResetView.as_view(template_name='accounts/resetPassword.html'), name='reset_password'),
     path('change-password/', PasswordChangeView.as_view(template_name='accounts/change_password.html'), name='change_password'),
 ]
