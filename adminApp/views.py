86fd3f7873a80486f4b7112840ddc396307f9e8f
Rodrigo Retamal <retamalpeirano@gmail.com>
2024-12-03 11:04:04 -0300

    Vamos súper bien.

--- adminApp/views.py
+++ adminApp/views.py
@@ -18,6 +18,9 @@ class DashboardView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['category_count'] = Category.objects.count()
+        context['low_stock_alerts'] = [
+            inventory.stock_minimun_message() for inventory in Inventory.objects.all() if inventory.almost_out()
+        ]
         return context
 
 """
