790d8afbf22d42ef74e9517a248a17356f388221
Rodrigo Retamal <retamalpeirano@gmail.com>
2024-12-05 12:38:34 -0300

    Seré un genio?

--- orders/urls.py
+++ orders/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create/', views.create_order, name='create_order'),
+    path('checkout/', views.checkout, name='checkout'),
     path('list/', views.list_orders, name='list_orders'),
     path('detail/<int:order_id>/', views.order_detail, name='order_detail'),
-    path('checkout/', views.checkout, name='checkout'),
-    path('create_order/', views.create_order, name='place_order'), 
+    path('order_success/<int:order_id>/', views.order_success, name='order_success'),
 ]
